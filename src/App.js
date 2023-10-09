409d9528fcf10c162bf4b449849dbd865de6b8da
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-10-09 14:54:13 +0530

    react-keys

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const IPL = ['CSK', 'MI', 'RCB', 'CSK'];
+
+  const result = IPL.map((ipl, index)=><h1>{ipl} index is {index}</h1>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{result}</h1>
     </div>
   );
 }
